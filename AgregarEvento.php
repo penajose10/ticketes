9daaf0d2d450c6c90a6c3438c11be5140ea91d41
alejandro <ingjaso@hotmail.com>
2014-01-23 00:38:25 -0430

    actualizacion alejandro 23/01/2014 1238am

--- AgregarEvento.php
+++ AgregarEvento.php
@@ -5,10 +5,24 @@ include_once Configuracion;
 include_once Smarty_Include;
 include_once Smarty_Conf;
 include_once Lugar;
+include_once Empleado;
+include_once Recinto;
 
+	$empleado=EMPLEADO::F_empleado();
+	$recinto=Recinto::RecintoN();
 	$pais= Lugar::lugar_N_P(); 	 
 	$smarty->assign("nombre_usuario", "DAVID");
 	$smarty->assign("pais", $pais);
+	$smarty->assign("empleados", $empleado);
+	$smarty->assign("Recintos",$recinto);
     $smarty->display(Template_Dir.'AgregarEvento.tpl'); 
+ 
 
+
+
+
+
+
+
+    
 ?>
